e4c7556d9faaa94a07f0d0e49e2259042acab524
Rimantas <belauzas@yahoo.com>
2022-01-28 20:39:29 +0200

    Read folder

--- components/servicesSection.js
+++ components/servicesSection.js
@@ -1,5 +1,10 @@
-function servicesSection() {
+const folder = require("../lib/folder.js");
+
+async function servicesSection() {
     // perskaitome kokie failai yra: /data/services folderyje
+    const files = await folder.read('data/services');
+    console.log(files);
+
     // gauname sarasa paslaugu JSON failu
     // visus JSON failus issiparsinti, t.y. konvertuoti i normalu JS objekta
     // atfiltruoti ir pasilikti tik validzias ir aktyvias paslaugas, t.y. ju objektus
